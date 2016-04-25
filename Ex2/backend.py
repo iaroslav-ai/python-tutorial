a06ba723147a1420c37e3c0f631fc6baeb8c2df0
Iaroslav Shcherbatyi <iaroslogos@gmail.com>
2016-04-25 18:48:07 +0200

    started exercise 2

---
+++ Ex2/backend.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def show_tensor(t):
+
+    for data in t:
+        fig, ax = plt.subplots()
+        ytick = range(data.shape[0])
+
+        if len(data.shape) < 2:
+            data = np.expand_dims(data, axis=1)
+            plt.ylabel("dim 0")
+            xtick = [' ']
+        else:
+            xtick = range(data.shape[1])
+            plt.ylabel("dim 0")
+            plt.xlabel("dim 1")
+
+        heatmap = ax.pcolor(data, cmap=plt.cm.Blues, edgecolor='black', linewidth=10, vmin = 0.0, vmax = 1.0)
+
+        fig.set_size_inches(data.shape[1] + 1.0, data.shape[0] + 1, forward=True)
+        # put the major ticks at the middle of each cell
+        ax.set_xticks(np.arange(data.shape[1]) + 0.5, minor=False)
+        ax.set_yticks(np.arange(data.shape[0]) + 0.5, minor=False)
+
+
+        # want a more natural, table-like display
+        ax.invert_yaxis()
+        ax.xaxis.tick_top()
+
+        ax.set_xticklabels(xtick, minor=False)
+        ax.set_yticklabels(ytick, minor=False)
+
+    plt.show()
